1b0bd5dfb2182a6eaecd2db8354204741d5db090
Crowdbotics <engineering@crowdbotics.com>
2024-03-22 20:50:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-feather-dev-134639",
+  url: "https://square-feather-dev-134639.botics.co",
+  api: "https://square-feather-dev-134639.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
